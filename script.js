9f0acb75e1c4f14bd73f68029d518cc7ff5bd02e
kyothiago <toberle2@hotmail.com>
2021-06-29 16:26:53 -0300

    Atualizado script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("Hello World!");
+
+var myName = "Thiago Oberle";
+
+let language = "JavaScript";
+
+const pattern = "ECMAScript";
+
+console.log(myName);
+
+var person = {
+    "name": "Thiago",
+    "age": 31
+}
+
+var students = ["Thiago", "João" "Maria"];
+
+function sum(a, b){
+    return a + b;
+}
+//typeof para verificar o tipo de dado da variável.
