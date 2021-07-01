b7bde7184e70a65314f4ddb58dd72622a066eea0
kyothiago <toberle2@hotmail.com>
2021-07-01 20:33:27 -0300

    Alguns exercícios básicos realizados

---
+++ exemplo1.js
@@ -0,0 +1,6 @@
+    let vlr = 10
+    const firstname = "thiago"
+    var idade = 31
+    console.log(vlr)
+    console.log(firstname)
+    console.log(idade)
--- script.js
+++ script.js
@@ -1,40 +1,37 @@
-//fizzbuzz
-//divisivel por 3 => "Fizz"
-//divisivel por 5 => "Buzz"
-//divisivel por 3 e 5 => "FizzBuzz"
-//Se não for um número => "Não é um número"
-//Se não for divisível nem por 3 e por 5 => valor da Entrada
+const listContainer = document.querySelector("[data-lists]")
+const newListForm = document.querySelector("[data-new-list-form]")
+const newListInput = document.querySelector("[data-new-list-input]")
 
-function fizzBuzz(entrada){
-     if(typeof entrada !== "number"){
-         return "Não é um número";
-     }
-     if((entrada % 3 === 0) && (entrada % 5 === 0)){
-         return "FizzBuzz";
-     }
-     if(entrada % 3 === 0){
-         return "Fizz";
-     }
-     if(entrada % 5 === 0){
-         return "Buzz";
-     }
-     else{
-         return entrada;
-     }
+let lists = [];
 
+newListForm.addEventListener("submit", function(e){
+    e.preventDefault()
+    const listName = newListInput.value;
+    if(listName === null || listName === '')
+    return;
+    const list = createList(listName);
+    newListInput.value = null;
+    lists.push(list);
+    render()
+})
+
+function createList(name){
+    return { id: Date.now().toString(), name: name }
 }
-let resultado = fizzBuzz(9)
 
-let newStr = '';
-function reverseAString(str){
-    for(let i = str.length -1; i >=0; i-- ){
-        newStr += str[i];
+function render(){
+     clearElement(listContainer)
+    lists.forEach(function(list) {
+        const item = document.createElement("li");
+        item.classList.add("item");
+        item.innerText = list.name;
+        listContainer.appendChild(item);
+    })
+}
+
+function clearElement(element){
+    while(element.firstChild){
+        element.removeChild(element.firstChild);
     }
-    console.log(newStr);
 }
 
-function convertToFahrenheit(value){
-    return value*9/5 +32;
-}
-
-let teste = convertToFahrenheit(5)
