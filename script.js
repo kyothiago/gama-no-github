4d6088ccd434da2e9f2cc729c5b727b8edb46d04
kyothiago <toberle2@hotmail.com>
2021-07-01 18:14:32 -0300

    primeiros exercícios javascript básico

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-console.log("Hello World!");
+//fizzbuzz
+//divisivel por 3 => "Fizz"
+//divisivel por 5 => "Buzz"
+//divisivel por 3 e 5 => "FizzBuzz"
+//Se não for um número => "Não é um número"
+//Se não for divisível nem por 3 e por 5 => valor da Entrada
 
-var myName = "Thiago Oberle";
+function fizzBuzz(entrada){
+     if(typeof entrada !== "number"){
+         return "Não é um número";
+     }
+     if((entrada % 3 === 0) && (entrada % 5 === 0)){
+         return "FizzBuzz";
+     }
+     if(entrada % 3 === 0){
+         return "Fizz";
+     }
+     if(entrada % 5 === 0){
+         return "Buzz";
+     }
+     else{
+         return entrada;
+     }
 
-let language = "JavaScript";
+}
+let resultado = fizzBuzz(9)
 
-const pattern = "ECMAScript";
-
-console.log(myName);
-
-var person = {
-    "name": "Thiago",
-    "age": 31
+let newStr = '';
+function reverseAString(str){
+    for(let i = str.length -1; i >=0; i-- ){
+        newStr += str[i];
+    }
+    console.log(newStr);
 }
 
-var students = ["Thiago", "João" "Maria"];
-
-function sum(a, b){
-    return a + b;
+function convertToFahrenheit(value){
+    return value*9/5 +32;
 }
-//typeof para verificar o tipo de dado da variável.
+
+let teste = convertToFahrenheit(5)
